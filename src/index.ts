98367723ff9483e64a2791e7a29cd329c17b5ea4
Steven Hobson-Campbell <xapphire13@live.com>
2018-01-03 00:00:37 -0800

    Removing need for handlebars

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,9 @@
 import * as express from "express";
-import * as handlebars from "handlebars";
 import * as path from "path";
 
 const APP_PATH = path.resolve(__dirname, "app");
-const HTML_TEMPLATE = handlebars.compile(`
-<html>
-  <head>
-    <title>{{title}}</title>
-  </head>
-  <body>
-    <script src="app/app.js"></script>
-  </body>
-</html>`);
-
 const app = express();
 
-app.use("/app", express.static(APP_PATH));
-
-app.get("/", (_req: express.Request, res:express.Response) => {
-  res.send(HTML_TEMPLATE({
-    title: "Xapphire13"
-  }));
-});
+app.use("/", express.static(APP_PATH));
 
 app.listen(80);
