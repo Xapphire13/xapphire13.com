79ac103ddafe70d4c22351d3e945f49e94b2935c
Steven <3392349+Xapphire13@users.noreply.github.com>
2018-01-04 13:53:11 -0800

    Add fork button and lorem ipsum

---
+++ src/types/react-github-button.d.ts
@@ -0,0 +1,14 @@
+declare module "react-github-button" {
+  import * as React from "react";
+
+  type Props = {
+    type: "stargazer" | "watchers" | "forks";
+    size?: "default" | "large";
+    namespace: string;
+    repo: string;
+  };
+
+  class GitHubButton extends React.Component<Props> {}
+
+  export = GitHubButton;
+}
