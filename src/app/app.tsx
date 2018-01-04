6c533e3da8f0021df4a00364b977ec3ab4664b2d
Steven <xapphire13@live.com>
2018-01-03 18:04:16 -0800

    Nav bar styling

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,8 +4,16 @@ import * as React from "react";
 export class App extends React.Component {
   public render(): JSX.Element {
     return <div className="app">
-      <header>
-        Xapphire13
+      <header className="app-header">
+        <div className="xapphire13-logo">
+          X13
+        </div>
+        <nav className="navigation-menu">
+          <div className="navigation-menu-content">
+            <div className="navigation-menu-item navigation-menu-item-1 selected">Home</div>
+            <div className="navigation-menu-item navigation-menu-item-2">Projects</div>
+          </div>
+        </nav>
       </header>
     </div>;
   }
