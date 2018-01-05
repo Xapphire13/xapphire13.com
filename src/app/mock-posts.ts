8ed159ba20b68da215ea567839bd4d31e9c60c4d
Steven Hobson-Campbell <3392349+Xapphire13@users.noreply.github.com>
2018-01-04 22:01:10 -0800

    Add post-preview

---
+++ src/app/mock-posts.ts
@@ -0,0 +1,125 @@
+export const MOCK_POSTS = [
+  "Test",
+  `
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+
+  \`\`\`
+  #include <stdio>
+
+  public void main() {
+    // Cool stuff
+
+    return 0;
+  }
+  \`\`\`
+
+  ![cat](http://bit.ly/2xZ9ILS)
+  `,
+  `
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+
+  \`\`\`
+  #include <stdio>
+
+  public void main() {
+    // Cool stuff
+
+    return 0;
+  }
+  \`\`\`
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+
+  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+  Nunc imperdiet vitae turpis nec pulvinar.
+  `
+];
---
+++ src/types/react-feather.d.ts
@@ -0,0 +1,7 @@
+declare module "react-feather" {
+  import * as React from "react";
+
+  export class Clock extends React.Component<React.HTMLAttributes<any>> {}
+  export class Edit extends React.Component<React.HTMLAttributes<any>> {}
+  export class BookOpen extends React.Component<React.HTMLAttributes<any>> {}
+}
