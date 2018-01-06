ebc4728fec3e440a5f4742aaf9193df048a6178f
Steven <xapphire13@live.com>
2018-01-05 17:52:13 -0800

    Splitting CSS text when using prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,13 +3,26 @@ const webpack = require("webpack");
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const config = require("./webpack.config.js");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const common = require("./webpack.common.js");
 
 const SRC_PATH = path.resolve(__dirname, "src/app");
 const DIST_PATH = path.resolve(__dirname, "dist/app");
 
-module.exports = merge(config, {
+module.exports = merge(common, {
+  module: {
+    rules: [
+      {
+        test: /\.less$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: ["css-loader", "less-loader"]
+        })
+      }
+    ]
+  },
   plugins: [
+    new ExtractTextPlugin("styles.css"),
     new UglifyJSPlugin({
       sourceMap: true
     }),
