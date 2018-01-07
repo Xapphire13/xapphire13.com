a717edff2b503529691dfe8618da2b57b05e36da
Steven Hobson-Campbell <3392349+Xapphire13@users.noreply.github.com>
2018-01-06 19:14:39 -0800

    Begin work on Client API

--- src/app/mock-posts.ts
+++ src/app/mock-posts.ts
@@ -1,125 +1,137 @@
-export default [
-  "Test",
-  `
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
+import {Post} from "./post";
+import {stripIndent} from "common-tags";
 
-  \`\`\`
-  #include <stdio>
+export default <Post[]>[
+  {
+    id: "foo",
+    markdownText: "Test"
+  },
+  {
+    id: "bar",
+    markdownText: stripIndent`
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
 
-  public void main() {
-    // Cool stuff
+    \`\`\`
+    #include <stdio>
 
-    return 0;
+    public void main() {
+      // Cool stuff
+
+      return 0;
+    }
+    \`\`\`
+
+    ![cat](http://bit.ly/2xZ9ILS)
+    `
+  },
+  {
+    id: "foobar",
+    markdownText: stripIndent`
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
+
+    \`\`\`
+    #include <stdio>
+
+    public void main() {
+      // Cool stuff
+
+      return 0;
+    }
+    \`\`\`
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
+
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
+    vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
+    purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
+    Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
+    Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
+    Nunc imperdiet vitae turpis nec pulvinar.
+    `
   }
-  \`\`\`
-
-  ![cat](http://bit.ly/2xZ9ILS)
-  `,
-  `
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
-
-  \`\`\`
-  #include <stdio>
-
-  public void main() {
-    // Cool stuff
-
-    return 0;
-  }
-  \`\`\`
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
-
-  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum vitae quam cursus,
-  vestibulum risus id, mollis neque. Nam sit amet consequat sapien. In ornare lectus in
-  purus semper interdum. Morbi aliquam ligula id ex ornare, nec ultrices nulla volutpat.
-  Aliquam erat volutpat. Sed tempus leo neque, nec consectetur sapien ornare sit amet.
-  Phasellus ut magna nec nulla fringilla sagittis a at dolor. Praesent quis iaculis metus.
-  Nunc imperdiet vitae turpis nec pulvinar.
-  `
 ];
