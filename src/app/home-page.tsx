8cad9782cfa62e8c40c45cce6c0ae4ca4a0e10a3
Steven Hobson-Campbell <xapphire13@live.com>
2018-01-06 18:55:32 -0800

    Implement infinite loader for posts

--- src/app/home-page.tsx
+++ src/app/home-page.tsx
@@ -1,14 +1,38 @@
 import "./styles/home-page.less";
 import * as React from "react";
 import {PostPreview} from "./post-preview";
-import MOCK_POSTS from "./mock-posts";
+import MP from "./mock-posts";
+import throttle = require("throttleit");
 
 const MAX_PREVIEW_LENGTH = 4000;
+const MOCK_POSTS = new Array(20).fill(null).reduce<string[]>(p => p.concat(...MP), []);
 
-export class HomePage extends React.Component {
+type State = {
+  loading: boolean;
+  loadedPosts: any[];
+};
+
+export class HomePage extends React.Component<{}, State> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      loading: false,
+      loadedPosts: []
+    };
+  }
+
+  public componentDidMount(): void {
+    window.addEventListener("scroll", this.onScroll)
+    this.loadPosts();
+  }
+
+  public componentWillUnmount(): void {
+    window.removeEventListener("scroll", this.onScroll);
+  }
+
   public render(): JSX.Element {
-    return <div className="home-page">
-      {MOCK_POSTS.map((post, i) =>
+    return <div className={`home-page${this.state.loading ? " loading" : ""}`}>
+      {this.state.loadedPosts.map((post, i) =>
         <PostPreview
           key={i}
           title="Test"
@@ -19,4 +43,30 @@ export class HomePage extends React.Component {
           maxLength={MAX_PREVIEW_LENGTH} />)}
     </div>;
   }
+
+  private loadPosts(): void {
+    const {loadedPosts} = this.state;
+    this.setState({
+      loading: true
+    });
+
+    for (
+      let i = loadedPosts.length, c = 3;
+      loadedPosts.length < MOCK_POSTS.length && c;
+      c--, i++) {
+      loadedPosts.push(MOCK_POSTS[i]);
+    }
+
+    this.setState({
+      loadedPosts,
+      loading: false
+    });
+  }
+
+  private onScroll = throttle(() => {
+    const app = document.getElementById("app")!;
+    if (window.innerHeight + window.scrollY + 200 >= app.offsetHeight) {
+      this.loadPosts();
+    }
+  }, 500);
 }
