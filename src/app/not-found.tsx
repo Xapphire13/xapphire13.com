b31ac8dc49b5de4151ce391a24d5db2d6e508d37
Steven <xapphire13@live.com>
2018-01-08 18:44:57 -0800

    Moved post fetching to server. Implemented basic continuation tokens

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,10 +1,14 @@
 import "./styles/not-found.less";
 import * as React from "react";
 
-export class NotFound extends React.Component {
+type Props = {
+  message?: string;
+};
+
+export class NotFound extends React.Component<Props> {
   public render(): JSX.Element {
     return <div className="not-found">
-      <h1>404: This is not the page you're looking for</h1>
+      <h1>{this.props.message || "404: This is not the page you're looking for"}</h1>
     </div>
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import * as express from "express";
 import * as path from "path";
+import {PostController} from "./post-controller";
 
 const APP_PATH = path.resolve(__dirname, "app");
 const app = express();
 
 app.set('port', process.env.PORT || 80);
+
+new PostController(app).registerRoutes();
 
 app.use("/app", express.static(APP_PATH));
 app.get("*", (_req, res) => {
