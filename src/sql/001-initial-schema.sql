1b98ce4c833757509fbd11a94bb684019a296882
Steven <xapphire13@live.com>
2018-01-09 19:04:26 -0800

    Hook up SQL queries

--- src/sql/001-initial-schema.sql
+++ src/sql/001-initial-schema.sql
@@ -1,8 +1,23 @@
 -- Up
 CREATE TABLE Post (
-  id INTEGER NOT NULL,
-  name TEXT,
-  PRIMARY KEY (id)
+  id INTEGER PRIMARY KEY,
+  title TEXT NOT NULL,
+  created TIME NOT NULL,
+  last_modified TIME NOT NULL,
+  markdown_text TEXT NOT NULL
+);
+
+CREATE TABLE Tag (
+  id INTEGER PRIMARY KEY,
+  name TEXT NOT NULL
+);
+
+CREATE TABLE PostTags (
+  post_id INTEGER NOT NULL,
+  tag_id INTEGER NOT NULL,
+  CONSTRAINT PK_PostTags PRIMARY KEY (post_id, tag_id),
+  CONSTRAINT FK_PostId FOREIGN KEY (post_id) REFERENCES Post(id),
+  CONSTRAINT FK_TagId FOREIGN KEY (tag_id) REFERENCES Tag(id)
 );
 
 -- Down
