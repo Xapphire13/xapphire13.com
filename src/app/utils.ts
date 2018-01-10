35cc07b19691d339b378e2fe4071547eae3faa6f
Steven Hobson-Campbell <xapphire13@live.com>
2018-01-09 20:50:12 -0800

    Adding Disposable interface and event subscription helpers

---
+++ src/app/utils.ts
@@ -0,0 +1,13 @@
+import {Disposable} from "./disposable";
+
+export function subscribeToEvent<K extends keyof WindowEventMap>(target: EventTarget, event: K, listener: (this: Window, ev: WindowEventMap[K]) => any): Disposable {
+  target.addEventListener(event, listener);
+
+  let dispose = () => {
+    target.removeEventListener(event, listener);
+    dispose = () => {};
+  };
+  return {
+    dispose
+  };
+}
