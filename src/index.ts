5199d4d6011ab767507204bbed326588a341f77d
Steven <xapphire13@live.com>
2018-01-12 12:32:45 -0800

    Adding tests

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import express = require("express");
 import bodyParser = require("body-parser");
 import * as path from "path";
 import * as sqlite from "sqlite";
-import {PostRepository} from "./post-repository";
+import {SqlPostRepository} from "./post-repository-sql";
 import {PostController} from "./post-controller";
 
 const APP_PATH = path.resolve(__dirname, "app");
@@ -21,7 +21,7 @@ async function main() {
   app.use(bodyParser.json());
 
   // Controllers
-  new PostController(app, new PostRepository(db)).registerRoutes();
+  new PostController(app, new SqlPostRepository(db)).registerRoutes();
 
   // Routes
   app.use("/app", express.static(APP_PATH));
