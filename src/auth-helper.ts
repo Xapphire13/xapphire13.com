668dd9aa704a343d9426b0516d03223249cc67ff
Steven <xapphire13@live.com>
2018-01-12 17:33:57 -0800

    protectedRoute helper

--- src/auth-helper.ts
+++ src/auth-helper.ts
@@ -1,3 +1,15 @@
+import * as express from "express";
+
 export function isAuthorized(): boolean {
   return process.env.NODE_ENV !== "production";
+}
+
+export function protectedRoute(handler: express.RequestHandler): express.RequestHandler {
+  return (req, res, next) => {
+    if (!isAuthorized()) {
+      return res.status(401).send();
+    }
+
+    return handler(req, res, next);
+  };
 }
