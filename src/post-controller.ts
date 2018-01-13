56397c88d668f5ffee993b0b40f653d717e79835
Steven <xapphire13@live.com>
2018-01-12 17:33:57 -0800

    protectedRoute helper

--- src/post-controller.ts
+++ src/post-controller.ts
@@ -1,7 +1,7 @@
 import {Express, Request as _Request, Response} from "express";
 import {Post} from "./post";
 import {PagedResponse} from "./paged-response";
-import {isAuthorized} from "./auth-helper";
+import {protectedRoute} from "./auth-helper";
 import {PostRepository} from "./post-repository";
 
 interface Request<TQuery = void, TParams = void, TBody = void> extends _Request {
@@ -18,26 +18,18 @@ export class PostController {
   public registerRoutes(): void {
     this.app.get("/api/posts", this.getPosts.bind(this));
     this.app.get("/api/posts/:id", this.getPost.bind(this));
-    this.app.post("/api/posts", this.postPost.bind(this));
-    this.app.patch("/api/posts/:id", this.patchPost.bind(this));
-    this.app.delete("/api/posts/:id", this.deletePost.bind(this));
+    this.app.post("/api/posts", protectedRoute(this.postPost.bind(this)));
+    this.app.patch("/api/posts/:id", protectedRoute(this.patchPost.bind(this)));
+    this.app.delete("/api/posts/:id", protectedRoute(this.deletePost.bind(this)));
   }
 
   private async postPost(req: Request<void, void, Post>, res: Response): Promise<Response> {
-    if (!isAuthorized()) {
-      return res.status(401).send();
-    }
-
     const post = await this.repository.createPost(req.body);
 
     return res.status(201).json(post);
   }
 
   private async patchPost(req: Request<void, {id: string}, Post>, res: Response): Promise<Response> {
-    if (!isAuthorized()) {
-      return res.status(401).send();
-    }
-
     const {id} = req.params;
 
     if (!await this.repository.getPost(+id)) {
@@ -52,10 +44,6 @@ export class PostController {
   }
 
   private async deletePost(req: Request<void, {id: string}, void>, res: Response): Promise<Response> {
-    if (!isAuthorized()) {
-      return res.status(401).send();
-    }
-
     const {id} = req.params;
 
     if (!await this.repository.getPost(+id)) {
