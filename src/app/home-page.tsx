ea864125b6cad160cdaa81df59b08ffcb3079d50
Steven Hobson-Campbell <3392349+Xapphire13@users.noreply.github.com>
2018-01-28 19:59:21 -0800

    Fixing post loading message. Fix/improve infinite load behavior

--- src/app/home-page.tsx
+++ src/app/home-page.tsx
@@ -24,8 +24,7 @@ type State = {
 
 export class HomePage extends React.Component<Props, State> {
   private scrollSubscription: Disposable = { dispose: () => {} };
-  private continuationToken: string | null;
-  private app: HTMLElement;
+  private ref: HTMLDivElement | null;
 
   constructor(props: Props) {
     super(props);
@@ -37,16 +36,20 @@ export class HomePage extends React.Component<Props, State> {
   }
 
   public async componentDidMount(): Promise<void> {
-    this.app = document.getElementById("app")!;
+    const isMore = await this.loadPosts();
 
-    this.continuationToken = await this.loadPosts();
-
-    while (this.continuationToken && this.app.clientHeight <= window.innerHeight) {
-      this.continuationToken = await this.loadPosts();
-    }
-
-    if (this.continuationToken) {
+    if (isMore) {
       this.scrollSubscription = Utils.subscribeToEvent(window, "scroll", this.onScroll);
+    }
+  }
+
+  public componentDidUpdate(_prevProps: Props, prevState: State): void {
+    if (prevState.loading && !this.state.loading && !this.state.allPostsLoaded) {
+      window.requestAnimationFrame(() => {
+        if (this.ref && this.ref.clientHeight <= window.innerHeight) {
+          this.loadPosts();
+        }
+      });
     }
   }
 
@@ -55,25 +58,24 @@ export class HomePage extends React.Component<Props, State> {
   }
 
   public render(): JSX.Element {
-    return <div className="home-page">
+    return <div className="home-page" ref={ref => this.ref = ref}>
       {this.props.user && <this.newPost />}
-      {this.state.loadedPosts.map((post, i) =>
-        <PostPreview
-          id={post.id}
-          key={i}
-          title={post.title}
-          created={new Date(post.created)}
-          lastModified={new Date(post.lastModified)}
-          markdownText={post.markdownText}
-          tags={post.tags}
-          maxLength={MAX_PREVIEW_LENGTH}
-          delete={async () => {
-            await ClientApi.deletePost(post.id);
-            this.setState({
-              loadedPosts: this.state.loadedPosts.filter(loadedPost => post.id != loadedPost.id)
-            });
-          }} />)}
-        <this.loadingMessage />
+      {this.state.loadedPosts.map(post => <PostPreview
+        key={post.id}
+        id={post.id}
+        title={post.title}
+        created={new Date(post.created)}
+        lastModified={new Date(post.lastModified)}
+        markdownText={post.markdownText}
+        tags={post.tags}
+        maxLength={MAX_PREVIEW_LENGTH}
+        delete={async () => {
+          await ClientApi.deletePost(post.id);
+          this.setState({
+            loadedPosts: this.state.loadedPosts.filter(loadedPost => post.id != loadedPost.id)
+          });
+        }} />)}
+      {this.loadingMessage()}
     </div>;
   }
 
@@ -81,40 +83,64 @@ export class HomePage extends React.Component<Props, State> {
     <PlusCircle className="new-post-icon"/>
   </div>;
 
-  private loadingMessage = (): JSX.Element => {
-    if (this.state.loading) {
-      return <p className="post-loading-status">Loading...</p>
-    } else if (this.state.loadedPosts.length === 0) {
-      return <p className="post-loading-status">No posts to load...</p>
+  private loadingMessage = (): JSX.Element | null => {
+    if (this.state.allPostsLoaded) {
+      if (this.state.loadedPosts.length === 0) {
+        return <p className="post-loading-status">No posts to load...</p>
+      }
+
+      return <p className="post-loading-status">Looks like you've reached the end...</p>
     }
 
-    return <p className="post-loading-status">Looks like you've reached the end...</p>
+    if (this.state.loading) {
+      return <p className="post-loading-status">Loading...</p>
+    }
+
+    return null;
   }
 
-  private async loadPosts(): Promise<string | null> {
-    const {loadedPosts} = this.state;
-    this.setState({
-      loading: true
-    });
+  private loadPosts = (() => {
+    let contToken: string | null;
+    let currentlyLoading = false;
 
-    const {continuationToken, values} = await ClientApi.getPosts(this.continuationToken);
+    return async (): Promise<boolean> => {
+      if (currentlyLoading) {
+        return !!contToken;
+      }
 
-    loadedPosts.push(...values);
+      currentlyLoading = true;
 
-    this.setState({
-      loadedPosts,
-      loading: false,
-      allPostsLoaded: !continuationToken
-    });
+      try {
+        if (contToken === null) {
+          return false;
+        }
 
-    return continuationToken;
-  }
+        const {loadedPosts} = this.state;
+        this.setState({
+          loading: true
+        });
+
+        const {continuationToken, values} = await ClientApi.getPosts(contToken);
+
+        loadedPosts.push(...values);
+
+        this.setState({
+          loadedPosts,
+          loading: false,
+          allPostsLoaded: !continuationToken
+        });
+
+        return !!(contToken = continuationToken || null);
+      } finally {
+        currentlyLoading = false;
+      }
+    };
+  })();
 
   private onScroll = throttle(async () => {
-    if (window.innerHeight + window.scrollY + 200 >= this.app.offsetHeight) {
-      if (this.continuationToken) {
-        this.continuationToken = await this.loadPosts();
-      } else {
+    const app = document.getElementById("app");
+    if (app && window.innerHeight + window.scrollY + 200 >= app.offsetHeight) {
+      if (!await this.loadPosts()) {
         this.scrollSubscription.dispose();
       }
     }
