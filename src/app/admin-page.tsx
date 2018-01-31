88e8b8329ed14e47dbed45269dca12a88d760368
Steven Hobson-Campbell <xapphire13@live.com>
2018-01-31 01:10:06 -0800

    WIP: Show logs in admin page

--- src/app/admin-page.tsx
+++ src/app/admin-page.tsx
@@ -1,20 +1,70 @@
 import "./styles/admin-page.less";
+import "react-table/react-table.css";
 import * as React from "react";
+import * as ClientApi from "./api/client-api";
+import ReactTable, {Column} from "react-table";
 import {RouteComponentProps} from "react-router-dom";
 import {User} from "./models/user";
+import {Log} from "../models/log";
 
 type Props = {
   user: User;
 } & RouteComponentProps<any>;
 
-export class AdminPage extends React.Component<Props> {
+type State = {
+  logs: Log[];
+  loading: boolean;
+  continuationToken?: string | null;
+};
+
+export class AdminPage extends React.Component<Props, State> {
+  private logColumns: Column[] = [
+    {
+      Header: "Timestamp",
+      accessor: "timestamp"
+    },
+    {
+      Header: "Level",
+      accessor: "level"
+    },
+    {
+      Header: "Message",
+      accessor: "message"
+    },
+    {
+      Header: "Exception",
+      accessor: "exception"
+    }
+  ];
+
   constructor(props: Props) {
     super(props);
+
+    this.state = {
+      logs: [],
+      loading: false
+    };
   }
 
   public render(): JSX.Element {
     return <div className="admin-page">
       Welcome {this.props.user.username}
+      <ReactTable
+        manual
+        data={this.state.logs}
+        loading={this.state.loading}
+        columns={this.logColumns}
+        onFetchData={async () => {
+          if (this.state.continuationToken !== null) {
+            this.setState({loading: true});
+            const page = await ClientApi.getLogs(this.state.continuationToken);
+            this.setState({
+              logs: this.state.logs.concat(page.values),
+              loading: false,
+              continuationToken: page.continuationToken
+            });
+          }
+        }}/>
     </div>;
   }
 }
---
+++ src/app/api/logs.ts
@@ -0,0 +1,16 @@
+import {Log} from "../../models/log";
+import {Page} from "../../pagination";
+
+export async function getLogs(continuationToken: string | null = null): Promise<Page<Log>> {
+  const response = await fetch(`/api/logs${continuationToken ? `?continue=${continuationToken}` : ""}`, {
+    headers: {
+      "Authorization": `Bearer ${window.localStorage.getItem("token")}`
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  return response.json();
+}
