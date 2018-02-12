5327f4201b0a76f08fdefb5104b45ea8ef9f243a
Steven Hobson-Campbell <3392349+Xapphire13@users.noreply.github.com>
2018-02-12 00:36:48 -0800

    Show stargazers count on repos. Show number of contributions

--- src/app/projects-page.tsx
+++ src/app/projects-page.tsx
@@ -1,11 +1,12 @@
 import "./styles/projects-page.less";
 import * as ClientApi from "./api/client-api";
 import * as React from "react";
+import {GitPullRequest, Star} from "react-feather";
 import {ScaleLoader} from "halogenium";
 
 type State = {
   ownedRepos?: ClientApi.GithubRepo[];
-  contributions?: ClientApi.GithubRepo[];
+  contributions?: ClientApi.RepoWithPrCount[];
   showLoading: boolean;
 };
 
@@ -19,8 +20,8 @@ export class ProjectsPage extends React.Component<any, State> {
   }
 
   public async componentDidMount(): Promise<void> {
-    ClientApi.getProjects().then(ownedRepos => this.setState({ownedRepos}));
-    ClientApi.getContributions().then(contributions => this.setState({contributions}));
+    ClientApi.getProjects().then(ownedRepos => this.setState({ownedRepos: ownedRepos.sort((a, b) => b.stargazers_count - a.stargazers_count)}));
+    ClientApi.getContributions().then(contributions => this.setState({contributions: contributions.sort((a, b) => b.prCount - a.prCount)}));
 
     // We don't want the loading indicator to flash on then off super quick
     // so we shall show the loading indicators for at least 500ms
@@ -30,10 +31,12 @@ export class ProjectsPage extends React.Component<any, State> {
   public render(): JSX.Element {
     return <div className="projects-page">
       <div>
-        <h2>Repositories</h2>
+        <h2>Repositories{!this.state.showLoading && this.state.ownedRepos && ` (${this.state.ownedRepos.length})`}</h2>
         {(this.state.ownedRepos && !this.state.showLoading) ?
           this.state.ownedRepos.map((repo, index) => <p key={index}>
-            <a href={repo.html_url} target="_blank">{repo.name}</a>{repo.description && ` - ${repo.description}`}
+            <a href={repo.html_url} target="_blank">{repo.name}</a>
+            {repo.description && ` - ${repo.description}`}
+            {" - "}{repo.stargazers_count}<Star className="icon" />
           </p>) :
           <div className="halogenium-container">
             <ScaleLoader className="halogenium-loader" />
@@ -41,10 +44,14 @@ export class ProjectsPage extends React.Component<any, State> {
       </div>
 
       <div>
-      <h2>Contributions</h2>
+      <h2>Contributions{!this.state.showLoading && this.state.contributions && ` (${this.state.contributions.reduce((agg, curr) => agg + curr.prCount, 0)})`}</h2>
         {(this.state.contributions && !this.state.showLoading) ?
-          this.state.contributions.map((repo, index) => <p key={index}>
-            <a href={repo.html_url} target="_blank"><span style={{fontWeight: "bold"}}>{repo.owner.login}</span>/{repo.name}</a>{repo.description && ` - ${repo.description}`}
+          this.state.contributions.map(({repo, prCount}, index) => <p key={index}>
+            <a href={repo.html_url} target="_blank">
+              <span style={{fontWeight: "bold"}}>{repo.owner.login}</span>/{repo.name}
+            </a>
+            {repo.description && ` - ${repo.description}`}
+            {" - "}{prCount}<GitPullRequest className="icon" />
           </p>) :
           <div className="halogenium-container">
             <ScaleLoader className="halogenium-loader" />
