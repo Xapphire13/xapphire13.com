7849307c60478b4553512f08b51e0db27d7fe869
Steven Hobson-Campbell <xapphire13@live.com>
2018-05-24 01:26:01 -0700

    fix: Update bundle script to work with monorepo

--- packages/server/src/constants.ts
+++ packages/server/src/constants.ts
@@ -1,4 +1,6 @@
 import * as path from "path";
 
-export const APP_PATH = path.resolve(__dirname, "../../client/dist");
+const IS_DEVELOPMENT = process.env.NODE_ENV !== "production";
+
+export const APP_PATH = path.resolve(__dirname, IS_DEVELOPMENT ? "../../client/dist" : "../client");
 export const PLAYGROUND_PATH = path.resolve(__dirname, "../playground");
--- scripts/bundle.ts
+++ scripts/bundle.ts
@@ -1,25 +1,43 @@
 import {ROOT_PATH} from "./config";
+import chalk from "chalk";
 import fs from "fs-extra";
 import globby from "globby";
 import path from "path";
 import process from "process";
 import yazl from "yazl";
 
-// TODO
-function bundle(outpath: string): void {
-  const distFiles = globby.sync(path.join("TODO", "**"));
-  const zipfile = new yazl.ZipFile();
+const CLIENT_PATH = path.join(ROOT_PATH, "packages/client");
+const SERVER_PATH = path.join(ROOT_PATH, "packages/server");
+const AZURE_INSTALL_SCRIPT = `
+  cd server
+  npm install --production --no-package-lock`;
 
-  distFiles.forEach(file => {
-    zipfile.addFile(file, path.relative(ROOT_PATH, file));
+async function bundle(outpath: string): Promise<void> {
+  const distFiles = await globby([
+    path.join(CLIENT_PATH, "dist/**"),
+    path.join(SERVER_PATH, "dist/**")
+  ]);
+
+  const zipfile = new yazl.ZipFile();
+  distFiles.forEach(filePath => {
+    zipfile.addFile(filePath, filePath.replace(/.+?\/([^/]+)\/dist\//i, "$1/"));
   });
 
-  zipfile.addFile("./package.json", "package.json");
-  zipfile.addFile("./web.config", "web.config");
+  zipfile.addFile(path.join(SERVER_PATH, "package.json"), "server/package.json");
+  zipfile.addFile(path.join(SERVER_PATH, "web.config"), "web.config");
+  zipfile.addBuffer(
+    new Buffer(AZURE_INSTALL_SCRIPT, "utf8"),
+    "install.ps1");
 
-  zipfile.outputStream.pipe(fs.createWriteStream(outpath)).on("close", () => console.log("Generated bundle"));
-  zipfile.end();
+  return new Promise<void>(res => {
+    zipfile.outputStream.pipe(fs.createWriteStream(outpath)).on("close", () => res());
+    zipfile.end();
+  });
 }
 
 const filename = process.argv[2] || "bundle.zip";
-bundle(filename);
+(async () => {
+  process.stdout.write(`Generating ${filename}... `);
+  await bundle(filename);
+  console.log(chalk.green("Done!"));
+})();
