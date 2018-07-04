f1fc1a8662f0fc9ea728cf337a85e6a8f1a2f0ba
Steven Hobson-Campbell <3392349+Xapphire13@users.noreply.github.com>
2018-07-03 21:39:47 -0700

    deployment: Remove old stuff from deploy file

--- deploy.cmd
+++ deploy.cmd
@@ -1,20 +1,4 @@
-:: Setup
-:: -----
-
-setlocal enabledelayedexpansion
-
 SET PATH=%PATH%;D:\home\tools
-SET ARTIFACTS=%~dp0%..\artifacts
-SET DEPLOYMENT_SOURCE=%~dp0%.
-SET DEPLOYMENT_TARGET=%~dp0%..\wwwroot
-
-IF NOT DEFINED NEXT_MANIFEST_PATH (
-  SET NEXT_MANIFEST_PATH=%ARTIFACTS%\manifest
-
-  IF NOT DEFINED PREVIOUS_MANIFEST_PATH (
-    SET PREVIOUS_MANIFEST_PATH=%ARTIFACTS%\manifest
-  )
-)
 
 call npm config set prefix "D:\home\tools"
 call npm i -g yarn
