74a1c16149777309dbd6224c064c1ba5b5d83580
Steven Hobson-Campbell <steven.hobson-campbell@airbnb.com>
2019-07-09 00:30:51 -0700

    Start to fix playground

--- src/server/controllers/playground-controller.ts
+++ src/server/controllers/playground-controller.ts
@@ -1,37 +1,36 @@
 import fs from "fs";
 import util from "util";
+import path from "path";
 import { Get, JsonController, Param } from "routing-controllers";
-import { PLAYGROUND_PATH } from "../constants";
+import { APP_PATH } from "../constants";
 import boom from "boom";
 import { injectable } from "tsyringe";
 import globby from "globby";
 import Experiment from "../entities/experiment";
-
-async function readJSON(path: string) {
-  return JSON.parse(await util.promisify(fs.readFile)(path, { encoding: "utf8" }));
-}
 
 @injectable()
 @JsonController("/api/playground")
 export class PlaygroundController {
   @Get("/experiments")
   public async getExperiments(): Promise<Experiment[]> {
-    const experiments = await globby([`${PLAYGROUND_PATH}/*/package.json`]);
+    const experiments = await globby([`${APP_PATH}/playground-*`]);
 
-    return Promise.all(experiments.map(async experiment => {
-      const { name, description, main } = await readJSON(experiment);
-
-      return { name, description, main };
-    }));
+    return Promise.all(
+      experiments.map(async experiment => {
+        return {
+          name: path.basename(experiment),
+          description: "TODO",
+          main: "TODO"
+        };
+      })
+    );
   }
 
   @Get("/experiments/:id")
   public async getExperiment(@Param("id") id: string): Promise<Experiment> {
-    try {
-      const { name, description, main } = await readJSON(`${PLAYGROUND_PATH}/${id}/package.json`);
-
-      return { name, description, main };
-    } catch {
+    if (await util.promisify(fs.exists)(`${APP_PATH}/playground-${id}}`)) {
+      return { name: id, description: "TODO", main: "TODO" };
+    } else {
       throw boom.notFound(`Can't find experiment with id: ${id}`);
     }
   }
