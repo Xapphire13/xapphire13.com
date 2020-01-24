656a41d208c36b64b128647c8c56c37f9ead5c57
Steven Hobson-Campbell <3392349+Xapphire13@users.noreply.github.com>
2020-01-23 23:19:26 -0800

    _id -> id

--- src/server/repositories/MongoUserRepository.ts
+++ src/server/repositories/MongoUserRepository.ts
@@ -1,17 +1,27 @@
 import { UserRepository } from "./user-repository";
 import User from ":entities/user";
 import { injectable, inject } from "tsyringe";
-import { Db as MongoDatabase } from "mongodb";
+import { Db as MongoDatabase, Collection } from "mongodb";
 
 @injectable()
 export default class MongoUserRepository implements UserRepository {
 
-  constructor(@inject("mongoDatabase") private db: MongoDatabase) { }
+  private userCollection: Collection<User>;
+
+  constructor(@inject("mongoDatabase") db: MongoDatabase) {
+    this.userCollection = db.collection("users");
+  }
 
   async getUser(username: string): Promise<User | null> {
-    return await this.db.collection("users").findOne({
+    const result = await this.userCollection.findOne({
       username
     });
+
+    if (result) {
+      result.id = (result as any)._id;
+    }
+
+    return result;
   }
 
   createUser(_username: string, _name: string): Promise<string> {
@@ -19,7 +29,7 @@ export default class MongoUserRepository implements UserRepository {
   }
 
   getUserCount(): Promise<number> {
-    return this.db.collection("users").count();
+    return this.userCollection.count();
   }
 
   addAdmin(_userId: string): Promise<void> {
@@ -27,19 +37,19 @@ export default class MongoUserRepository implements UserRepository {
   }
 
   async storeTokenSecret(userId: string, secret: string): Promise<void> {
-    await this.db.collection("users").updateOne({ _id: userId }, { tokenSecret: secret });
+    await this.userCollection.updateOne({ _id: userId }, { tokenSecret: secret });
   }
 
   async storePasswordHash(userId: string, hash: string): Promise<void> {
-    await this.db.collection("users").updateOne({ _id: userId }, { passwordHash: hash });
+    await this.userCollection.updateOne({ _id: userId }, { passwordHash: hash });
   }
 
   async storeAuthenticatorSecret(userId: string, secret: string): Promise<void> {
-    await this.db.collection("users").updateOne({ _id: userId }, { authenticatorSecret: secret });
+    await this.userCollection.updateOne({ _id: userId }, { authenticatorSecret: secret });
   }
 
   async isAdmin(userId: string): Promise<boolean> {
-    const user = await this.db.collection<User>("users").findOne({ _id: userId });
+    const user = await this.userCollection.findOne({ _id: userId });
 
     return !!user && user.isAdmin;
   }
