b207b04a505c5ea3ad16b0de025915260c86c860
Steven Hobson-Campbell <xapphire13@live.com>
2020-01-26 19:20:50 -0800

    Update deps. make config sync

--- src/server/config.ts
+++ src/server/config.ts
@@ -1,24 +1,8 @@
-import fs from "fs";
-import util from "util";
-
-type ConfigFile = {
-  githubToken?: string;
-};
-
 export class Config {
-  private configFile: ConfigFile;
 
-  constructor(private configFilePath: string) { }
+  constructor() { }
 
   public get githubToken(): string | undefined {
-    return this.configFile.githubToken;
-  }
-
-  public async initialize(): Promise<void> {
-    try {
-      this.configFile = JSON.parse(await util.promisify(fs.readFile)(this.configFilePath, { encoding: "utf8" }));
-    } catch {
-      this.configFile = {};
-    }
+    return process.env["GITHUB_TOKEN"];
   }
 }
