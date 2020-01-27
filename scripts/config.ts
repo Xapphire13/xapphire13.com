d40c4875a7fc7f45bed41c851e2235ade6793d61
Steven Hobson-Campbell <3392349+Xapphire13@users.noreply.github.com>
2020-01-26 18:29:07 -0800

    remove unused files

--- scripts/config.ts
+++
@@ -1,4 +0,0 @@
-import path from "path";
-
-export const ROOT_PATH = path.resolve(__dirname, "..");
-export const WWWROOT_PATH = path.resolve(ROOT_PATH, "../wwwroot");
--- src/server/sql/003-draft-posts.sql
+++
@@ -1,8 +0,0 @@
--- Up
-ALTER TABLE Post
-ADD COLUMN is_published INTEGER NOT NULL DEFAULT 0;
-
-UPDATE Post
-SET is_published = 1;
-
--- Down
