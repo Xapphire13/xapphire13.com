eead8b66956af6f8cd1e01aa4e542ebdc009c98a
Steven Hobson-Campbell <3392349+Xapphire13@users.noreply.github.com>
2020-01-27 22:19:39 -0800

    use username rather than id

--- src/server/repositories/UserRepository.ts
+++ src/server/repositories/UserRepository.ts
@@ -2,8 +2,8 @@ import User from "../entities/user";
 
 export interface UserRepository {
   getUser(username: string): Promise<User | null>;
-  storeTokenSecret(userId: string, secret: string): Promise<void>;
-  storePasswordHash(userId: string, hash: string): Promise<void>;
-  storeAuthenticatorSecret(userId: string, secret: string): Promise<void>;
-  isAdmin(userId: string): Promise<boolean>;
+  storeTokenSecret(username: string, secret: string): Promise<void>;
+  storePasswordHash(usename: string, hash: string): Promise<void>;
+  storeAuthenticatorSecret(username: string, secret: string): Promise<void>;
+  isAdmin(username: string): Promise<boolean>;
 }
