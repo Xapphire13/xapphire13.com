1e7a2bf54a90d57117416279e05731542b8c3fd7
Steven Hobson-Campbell <3392349+Xapphire13@users.noreply.github.com>
2020-02-03 00:00:28 -0800

    lint

--- src/server/repositories/MongoUserRepository.ts
+++ src/server/repositories/MongoUserRepository.ts
@@ -1,7 +1,7 @@
 import { injectable, inject } from 'tsyringe';
 import { Db as MongoDatabase, Collection } from 'mongodb';
-import { UserRepository } from './UserRepository';
 import User from 'src/server/models/user';
+import { UserRepository } from './UserRepository';
 
 @injectable()
 export default class MongoUserRepository implements UserRepository {
